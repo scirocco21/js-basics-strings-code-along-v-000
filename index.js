a8b7f2efd198ba57d876bf29f2e4af43447ef8b3
Seb Gertz <sebgertz@gmail.com>
2018-04-11 18:30:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ let currentUser = "Seb";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = `Welcome to Flatbook, ${currentUser}!`.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
