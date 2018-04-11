cae2a01447261cdc4a2fceb2e97f7aa2b283d2fb
Seb Gertz <sebgertz@gmail.com>
2018-04-11 18:19:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 let currentUser = "Seb";
 
-const welcomeMessage = "Welcome to Flatbook, #{currentUser}!";
+const welcomeMessage = "Welcome to Flatbook, ${currentUser}!";
